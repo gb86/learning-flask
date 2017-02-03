99b06f52e10c0a432d08b85fd212a7789b7dc56d
Gopi <gopi.balasingam@reedonline.co.uk>
2017-02-03 15:28:48 +0000

    routes updated

--- routes.py
+++ routes.py
@@ -7,5 +7,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/api/getteams")
+def get_teams():
+    teams = [
+        "Tottenham",
+        "Chelsea",
+        "Arsenal",
+        "Manchester United",
+        "Manchester City",
+        "Liverpool",
+        "Everton"
+    ]
+
+    return jsonify(teams)
+
 if __name__ == "__main__":
     app.run(debug=True)
