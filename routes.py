54037f8458b4bf559b4a7a240dac785ac6a9b698
Gopi <gopi.balasingam@reedonline.co.uk>
2017-02-03 16:18:44 +0000

    update

--- routes.py
+++ routes.py
@@ -8,6 +8,35 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/api/searchfilms/<name>")
+def search_films(name):
+    films = [
+        "The Shawshank Redemption",
+        "The Godfather",
+        "The Godfather: Part II",
+        "Il buono, il brutto, il cattivo.",
+        "Pulp Fiction",
+        "Inception",
+        "Schindler's List",
+        "12 Angry Men",
+        "One Flew Over the Cuckoo's Nest",
+        "The Dark Knight",
+        "Star Wars: Episode V - The Empire Strikes Back",
+        "The Lord of the Rings: The Return of the King",
+        "Shichinin no samurai",
+        "Star Wars",
+        "Goodfellas"
+    ]
+
+    results = []
+
+    for f in films:
+        if f.lower().startswith(name.lower()):
+            results.append(f)
+
+    return jsonify(results)
+
+
 @app.route("/api/getteams")
 def get_teams():
     teams = [
